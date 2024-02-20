9632dca09eae9a01543caa428b58ded2288e1d34
amypo-reactapp <reactapp@amypo.in>
2024-02-20 08:44:54 +0000

    t

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div>hello, world</div>;
+  return <div>hello, world!</div>;
 }
 
 export default App;
--- src/Sena.js
+++ src/Sena.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function Sena() {
-  return <div>hello, world</div>;
+  return <div>hello, world!</div>;
 }
 
 export default Sena;
